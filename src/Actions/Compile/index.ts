bce1a4016dcd2a229ff27d5fa7e173336911fae2
Vic van Cooten <duveaux1@gmail.com>
2020-07-11 15:08:33 +0200

    Un

--- src/Actions/Compile/index.ts
+++ src/Actions/Compile/index.ts
@@ -31,11 +31,6 @@ module.exports = async (args, models) => {
       );
 
       shell.cd(`/AppBox/Files/Sites/Source/${args.siteId}`);
-      // Todo why is this needed?
-      shell.exec("rm -rf node_modules");
-      shell.exec("rm package-lock.json");
-      shell.exec("npm cache clear --force");
-      shell.exec("npm install");
 
       shell.exec("gatsby build");
 
