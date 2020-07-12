f05c591515ee7a119369439bcad1ff3086af2bf9
Vic van Cooten <duveaux1@gmail.com>
2020-07-12 14:17:44 +0200

    Also add connected data to json file.

--- src/Actions/Compile/index.ts
+++ src/Actions/Compile/index.ts
@@ -13,11 +13,22 @@ module.exports = async (args, models) => {
   // Get pages metadata
   const pageModel = await models.objects.findOne({ key: "publisher-pages" });
 
+  const otherObjects = {};
+
+  await (site.data.data || []).reduce(async (previousPromise, dataBlock) => {
+    let newData = await previousPromise;
+
+    const model = await models.objects.findOne({ key: dataBlock });
+    otherObjects[dataBlock] = model.fields;
+
+    return model;
+  }, Promise.resolve([]));
+
   const configData = {
     data: site.data.siteSettings,
     menus: site.data.menus,
     baseUrl: "http://localhost:8600",
-    objects: { "publisher-pages": pageModel.fields },
+    objects: { "publisher-pages": pageModel.fields, ...otherObjects },
   };
 
   fs.writeFile(
